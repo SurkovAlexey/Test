2b4d3fd004e7f898cc5bb004243b90b48ad990f5
SurkovAlexey <133164655+SurkovAlexey@users.noreply.github.com>
2023-05-12 02:52:21 +0300

    Моя лабораторная 22

---
+++ Project1/Project1.pas
@@ -0,0 +1,10 @@
+﻿{$apptype windows}
+uses Unit1;
+
+begin
+ 
+  System.Windows.Forms.Application.EnableVisualStyles();
+  System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+  System.Windows.Forms.Application.Run(new Form1)
+  
+end.
