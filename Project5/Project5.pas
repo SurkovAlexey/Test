edbe697cb65c120f6920493139fece753161f53e
Alexey Surkov <133164655+SurkovAlexey@users.noreply.github.com>
2023-05-24 03:13:54 +0300

    Delete Project5 directory

--- Project5/Project5.pas
+++
@@ -1,7 +0,0 @@
-﻿uses Unit1;
-
-begin
-  System.Windows.Forms.Application.EnableVisualStyles();
-  System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-  System.Windows.Forms.Application.Run(new Form1)
-end.
